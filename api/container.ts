0d4055bb44f83c77b1991266d7e7ee7098376f76
alvarosw <alvarossmiguel@gmail.com>
2023-09-13 16:57:29 -0300

    feat: prisma setup and employee model creation

--- api/container.ts
+++ api/container.ts
@@ -1,8 +1,18 @@
-import { createContainer } from 'awilix';
+import { asClass, asValue, createContainer } from 'awilix';
 import { loadControllers, scopePerRequest } from 'awilix-express';
+
+import { PrismaClient } from '@prisma/client';
+import EmployeeService from './src/services/EmployeeService';
 
 const container = createContainer({
   injectionMode: 'CLASSIC',
+});
+
+const prisma = new PrismaClient();
+
+container.register({
+  prisma: asValue(prisma),
+  employeeService: asClass(EmployeeService)
 });
 
 export const dependencyInjectionRequestScope = scopePerRequest(container);
