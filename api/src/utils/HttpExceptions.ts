5eff5efecfdba4a2af6a55b696c8e9969439fdd3
alvarosw <alvarossmiguel@gmail.com>
2023-09-13 23:52:19 -0300

    feat: exception classes

---
+++ api/src/utils/HttpExceptions.ts
@@ -0,0 +1,17 @@
+export class HttpException extends Error {
+  constructor(public statusCode: number, errorMessage: string) {
+    super(errorMessage);
+  }
+}
+
+export class UnprocessableEntity extends HttpException {
+  constructor(errorMessage: string) {
+    super(422, errorMessage);
+  }
+}
+
+export class InternalServerError extends HttpException {
+  constructor(errorMessage: string) {
+    super(500, errorMessage);
+  }
+}
