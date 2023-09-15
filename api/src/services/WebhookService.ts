9403b9e92f660399190c9ca72ffdb7ee83e6fc73
alvarosw <alvarossmiguel@gmail.com>
2023-09-15 13:15:09 -0300

    feat: change webhook endpoint

--- api/src/services/WebhookService.ts
+++ api/src/services/WebhookService.ts
@@ -10,7 +10,7 @@ export default class WebhookService {
   }
 
   async postEmployee(data: EmployeeContact | EmployeeContact[]) {
-    return this.apiInstance.post('webhook', data).catch(error => {
+    return this.apiInstance.post('contacts/webhook', data).catch(error => {
       if (error instanceof AxiosError)
         console.error('Error sending postEmployee webhook:', error.message);
       else
